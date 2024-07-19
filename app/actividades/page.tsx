cbd8055592f2ddd4b77a046374bfa5d2c5c79e2d
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-07-19 10:59:05 -0500

    Day 4 - Add Contact View On All Pages

--- app/actividades/page.tsx
+++ app/actividades/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SectionA from "../views/Actividades/sectionA";
+import Contacto from "../views/Shared/Contacto";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <SectionA />
+      <Contacto />
     </main>
   );
 }
